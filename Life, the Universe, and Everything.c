3e8d43d9275eccd8064e137f092e091cafc403f5
Subhankar Pal <subho.playboy10@gmail.com>
2020-07-24 03:14:37 +0530

    Create Life, the Universe, and Everything.c

---
+++ Life, the Universe, and Everything.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    while(1){
+        int n;
+        scanf("%d", &n);
+        if(n==42)
+            break;
+        printf("%d\n",n);
+    }
+	return 0;
+}
